9aa8b5e54eb4de8a226508552ad0d5ca5818c63c
danilbutakov <12021109359@irgups.ru>
2022-10-22 05:56:25 +0800

    Переделал отсупы в стилях

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,4 @@
 import { CapacitorConfig } from '@capacitor/cli';
-import { SplashScreen } from '@capacitor/splash-screen';
-
-await SplashScreen.hide();
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
