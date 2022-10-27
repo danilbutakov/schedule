745013b2b3480a111716acba83cbe9bfce05db40
danilbutakov <12021109359@irgups.ru>
2022-10-27 17:11:23 +0800

    Переделывал splash screen и logo приложения

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,19 +1,19 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ScheduleApp',
-  appName: 'ScheduleApp',
-  webDir: 'build',
-  bundledWebRuntime: false,
-  android: {
-   path: 'android',
-  },
-  ios: {
-   path: 'android',
-  },
-  server: {
-   url: 'https://schedule-psi.vercel.app/',
-  }
+   appId: 'ScheduleApp',
+   appName: 'ScheduleApp',
+   webDir: 'build',
+   bundledWebRuntime: false,
+   android: {
+      path: 'android',
+   },
+   ios: {
+      path: 'android',
+   },
+   server: {
+      url: 'https://schedule-psi.vercel.app/',
+   }
 };
 
 export default config;
