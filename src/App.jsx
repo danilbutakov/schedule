870fc2366226828753f8b1a35e14f71bc1a73be2
danilbutakov <12021109359@irgups.ru>
2022-10-29 02:00:53 +0800

    Test Firebase

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,30 @@ import { LoginContainer } from './components/Login/LoginContainer';
 import Home from './pages/Home';
 import Search from './components/Search/Search';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyDxldqcf9v0t7MVSZDkSkU-YidtRaYojPI',
+	authDomain: 'schedule-dc1ed.firebaseapp.com',
+	databaseURL:
+		'https://schedule-dc1ed-default-rtdb.europe-west1.firebasedatabase.app',
+	projectId: 'schedule-dc1ed',
+	storageBucket: 'schedule-dc1ed.appspot.com',
+	messagingSenderId: '189433836505',
+	appId: '1:189433836505:web:04d0b9865200bdc2619fc8',
+	measurementId: 'G-4RHMTFPCDR',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
 	const AnimatedSwitch = () => {
 		const location = useLocation();
