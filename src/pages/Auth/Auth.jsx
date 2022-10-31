8ac35f9dbfbcc387aa3f34c18628d73dcc21d5c7
danilbutakov <12021109359@irgups.ru>
2022-10-31 08:53:56 +0800

    Убрал авторизацию и анимацию logoShapes из onBoard

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -9,37 +9,37 @@ import { useState } from 'react';
 
 export const Auth = () => {
 	const [user, loading] = useAuthState(auth);
-	const navigate = useNavigate();
+	// const navigate = useNavigate();
 
-	const googleProvider = new GoogleAuthProvider();
-	const GoogleLogin = async () => {
-		try {
-			const result = await signInWithPopup(auth, googleProvider);
-			console.log(result.user);
-			navigate('/login');
-		} catch (error) {
-			console.log(error);
-		}
-	};
+	// const googleProvider = new GoogleAuthProvider();
+	// const GoogleLogin = async () => {
+	// 	try {
+	// 		const result = await signInWithPopup(auth, googleProvider);
+	// 		console.log(result.user);
+	// 		navigate('/login');
+	// 	} catch (error) {
+	// 		console.log(error);
+	// 	}
+	// };
 
-	useEffect(() => {
-		if (user) {
-			navigate('/home');
-		} else {
-			navigate('/onBoard');
-		}
-		if (!user) {
-			navigate('/auth');
-		} else {
-			navigate('/home');
-		}
-	}, [user]);
+	// useEffect(() => {
+	// 	if (user) {
+	// 		navigate('/home');
+	// 	} else {
+	// 		navigate('/onBoard');
+	// 	}
+	// 	if (!user) {
+	// 		navigate('/auth');
+	// 	} else {
+	// 		navigate('/home');
+	// 	}
+	// }, [user]);
 
 	return (
 		<div>
 			<div>
 				<h1>Введите название ВУЗа</h1>
-				<button onClick={GoogleLogin}>Google</button>
+				{/* <button onClick={GoogleLogin}>Google</button> */}
 			</div>
 		</div>
 	);
