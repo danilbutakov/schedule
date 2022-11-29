6a4b6dde6d49467f4425398f0bdd45686308c5bc
danilbutakov <12021109359@irgups.ru>
2022-11-29 21:35:09 +0800

    исправил конфликты мерджа ветки Дани в main

--- app/screens/Search/SearchTeachers.jsx
+++ app/screens/Search/SearchTeachers.jsx
@@ -1,40 +1,56 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 import Teachers from '../../../assets/svgUtils/Teachers.svg';
+
+const { height } = Dimensions.get('screen');
 
 const SearchTeachers = () => {
 	return (
 		<View style={styles.searchContainer}>
 			<View style={styles.searchGroupInfo}>
 				<View style={styles.searchText}>
-					<Text>ПРЕПОДАВАТЕЛЬ</Text>
-					<Text>Попова Наталья Сергеевна</Text>
-					<Text>Кафедра: «Математика», ауд. Г-203</Text>
+					<Text style={{ paddingBottom: 8, fontFamily: 'Montserrat-Bold' }}>
+						ПРЕПОДАВАТЕЛЬ
+					</Text>
+					<Text style={{ paddingBottom: 4, fontFamily: 'Montserrat-SemiBold' }}>
+						Попова Наталья Сергеевна
+					</Text>
+					<Text style={{ color: '#8E8E93', fontFamily: 'Montserrat-Medium' }}>
+						Кафедра: «Математика», ауд. Г-203
+					</Text>
 				</View>
 				<View style={styles.searchImg}>
-					<Teachers width={45} height={45} />
+					<Teachers width={60} height={60} />
 				</View>
 			</View>
 			<View style={styles.searchDate}>
-				<Text>ЧЕТВЕРГ, 27 ОКТЯБРЯ</Text>
+				<Text style={{ color: '#8E8E93', fontFamily: 'Montserrat-SemiBold' }}>
+					ЧЕТВЕРГ, 27 ОКТЯБРЯ
+				</Text>
 			</View>
 			<View style={styles.pairsContainer}>
 				<View style={styles.pair}>
 					<View style={styles.headPair}>
 						<View style={styles.headLeft}>
-							<Text>Лабораторная</Text>
+							<Text style={{ fontFamily: 'Montserrat-Medium' }}>
+								Лабораторная
+							</Text>
 						</View>
-						<Text>13:50 - 15:20</Text>
+						<Text style={{ fontFamily: 'Montserrat-Regular' }}>
+							13:50 - 15:20
+						</Text>
 					</View>
 					<View style={styles.infoPair}>
 						<View style={styles.namePair}>
-							<Text>ООП</Text>
+							<Text style={{ fontFamily: 'Montserrat-SemiBold' }}>ООП</Text>
 						</View>
 						<View style={styles.teachPair}>
-							<Text>ПИ.1-20-1</Text>
+							<Text style={{ fontFamily: 'Montserrat-Regular' }}>
+								ПИ.1-20-1
+							</Text>
 						</View>
 						<View style={styles.classRoomPair}>
-							<Text>Д-418</Text>
+							<Text style={{ fontFamily: 'Montserrat-Regular' }}>Д-418</Text>
 						</View>
 					</View>
 				</View>
@@ -44,20 +60,62 @@ const SearchTeachers = () => {
 };
 
 const styles = StyleSheet.create({
-	searchContainer: {},
+	searchContainer: {
+		width: '100%',
+		height,
+		backgroundColor: '#F8F8F8',
+		padding: 5
+	},
 	searchTitle: {},
-	searchGroupInfo: {},
+
+	searchGroupInfo: {
+		backgroundColor: '#FFFFFF',
+		height: 120,
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		paddingLeft: 20,
+		paddingRight: 20,
+		borderRadius: 16
+	},
 	searchText: {},
 	searchImg: {},
-	searchDate: {},
-	pairsContainer: {},
-	pair: {},
-	headPair: {},
+	searchDate: {
+		paddingBottom: 12,
+		paddingTop: 12,
+		paddingLeft: 20,
+		paddingRight: 20
+	},
+	pairsContainer: {
+		borderRadius: 16,
+		backgroundColor: '#FFFFFF',
+		height: 115,
+		width: '100%'
+	},
+	pair: {
+		paddingTop: 18,
+		paddingLeft: 22,
+		paddingRight: 22,
+		paddingBottom: 11
+	},
+	headPair: {
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		paddingBottom: 6
+	},
 	headLeft: {},
 	infoPair: {},
-	namePair: {},
-	teachPair: {},
-	classRoomPair: {}
+	namePair: {
+		paddingBottom: 3
+	},
+	teachPair: {
+		paddingBottom: 5
+	},
+	classRoomPair: {
+		paddingBottom: 11
+	}
 });
 
 export default SearchTeachers;
