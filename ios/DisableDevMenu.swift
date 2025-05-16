cac483e3832ad775d51252a7dc308fbde1fe1a89
danilbutakov <ibutakov52@gmail.com>
2025-05-16 13:17:41 +0800

    fix: add ios folder and Xcode project for EAS build

---
+++ ios/DisableDevMenu.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+#if DISABLE_EXPO_DEV_MENU
+// Этот файл нужен для отключения expo-dev-menu в режиме сборки
+// Он используется для исправления ошибок компиляции с expo-dev-menu-interface
+#endif
--- ios/Schedulle/AppDelegate.h
+++ ios/Schedulle/AppDelegate.h
@@ -1,9 +1,8 @@
-#import <Foundation/Foundation.h>
-#import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
+#import <React/RCTBridgeDelegate.h>
 
-#import <Expo/Expo.h>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
-@interface AppDelegate : EXAppDelegateWrapper <RCTBridgeDelegate>
+@property (nonatomic, strong) UIWindow *window;
 
 @end
